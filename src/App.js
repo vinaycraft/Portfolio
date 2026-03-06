ff27ea37c4a394e889542deb34654aed788b61e4
vinay patil <vinay100876@gmail.com>
2026-03-06 18:03:23 +0530

    Initial commit: React portfolio website with modern design with profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import profileImage from './assets/profile.jpg';
+import profileImage from './assets/profile.png';
 
 const App = () => {
   const skills = [
